cd123fccf72c4604ca9daf4903749ac155c38fc6
mathieu <mathieu.hautebas@gmail.com>
2018-10-22 16:13:36 -0400

    add tabs check

--- normes.py
+++ normes.py
@@ -16,6 +16,24 @@ def check_for_spaces(c_file):
             i += 1
             if line.endswith(" \n"):
                 print("space at the EoL : %d" % i)
+"""
+def check_for_var_dec(c_file):
+    with open(file_to_test, 'r') as f:
+        lines = f.readlines()
+        for i in range(0, len(lines)):
+            for i in range(0, len(lines[i]):
+                if 
+"""
+
+def check_for_tabs(c_file):
+    with open(c_file, "r") as file:
+        this = file.read()
+        line = 0
+        for i in range(0, len(this)):
+            if this[i] == '\n':
+                line += 1
+            if this[i] == '\t':
+                print("found tab line : %d" % line)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -23,4 +41,5 @@ if __name__ == '__main__':
         sys.exit(1)
 
     file_to_test = sys.argv[1]
-    check_for_spaces(file_to_test)
+    check_for_tabs(file_to_test)
+    #check_for_spaces(file_to_test)
