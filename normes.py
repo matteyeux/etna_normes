49b86dfd758dc013f6d6730a17ce423f53dd9671
mathieu <mathieu.hautebas@gmail.com>
2018-10-23 15:05:15 -0400

    check for etna header

--- normes.py
+++ normes.py
@@ -8,8 +8,22 @@
 
 import sys
 
+#def check_for_variable_declaration(c_file):
+    # check if there is one variable
+    # declaration per line
+
+# In ETNA's pool you have to set a specific
+# header with your code
+def check_for_editor_header(c_file):
+     with open(c_file, 'r') as f:
+        lines = f.readlines()
+        for i in range(0, len(lines)):
+            if "ETNA PROJECT," in lines[i]:
+                return True
+        return False
+
 def check_for_spaces(c_file):
-    with open(file_to_test, 'r') as f:
+    with open(c_file, 'r') as f:
         lines = f.readlines()
         i = 0
         for line in lines:
@@ -45,6 +59,10 @@ if __name__ == '__main__':
         sys.exit(1)
 
     file_to_test = sys.argv[1]
+
+    if check_for_editor_header(file_to_test) != True:
+        print("[ERROR] ETNA header not found")
+    
     check_for_tabs(file_to_test)
     check_for_spaces(file_to_test)
     check_for_return(file_to_test)
