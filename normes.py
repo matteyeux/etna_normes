e2626f9fcabb7679da003dac2a3ab2b5e7aafe24
mathieu <mathieu.hautebas@gmail.com>
2018-10-26 11:17:30 -0400

    add another set of checks

--- normes.py
+++ normes.py
@@ -6,13 +6,27 @@
 ##     check for ETNA normes. Love u Samantha <3
 ##
 
-
 # TODO : open file once, not in every function
 
 import sys
 
 # In ETNA's pool you have to set a specific
 # header with your code
+
+def check_needed_spaces(c_file):
+    with open(c_file, "r") as f:
+        lines = f.readlines()
+        for i in range(0, len(lines)):
+            if "){" in lines[i]:
+                print("  [line %d] no space before curly bracket" % (i+1))
+            if "if(" in lines[i]:
+                print("  [line %d] no space between if and parenthese" % (i+1))
+            if "for(" in lines[i]:
+                print("  [line %d] no space between for and parenthese" % (i+1))
+            if "while(" in lines[i]:
+                print("  [line %d] no space between while and parenthese" % (i+1))
+
+
 def check_for_editor_header(c_file):
      with open(c_file, 'r') as f:
         lines = f.readlines()
@@ -28,7 +42,7 @@ def check_for_spaces(c_file):
         for line in lines:
             i += 1
             if line.endswith(" \n"):
-                print("[line %d] space at EoL" % i)
+                print("  [line %d] space at EoL" % i)
 
 def check_for_return(c_file):
     with open(c_file, "r") as f:
@@ -38,9 +52,9 @@ def check_for_return(c_file):
                 if "return (" in lines[i]:
                     True #print("good")
                 elif "return(" in lines[i]:
-                    print("[line %d]  no space after return" % (i+1))
+                    print("  [line %d]  no space after return" % (i+1))
                 else:
-                    print("[line %d]  no parentheses found for return" % (i+1))
+                    print("  [line %d]  no parentheses found for return" % (i+1))
 
 def check_for_tabs(c_file):
     with open(c_file, "r") as file:
@@ -50,17 +64,18 @@ def check_for_tabs(c_file):
             if this[i] == '\n':
                 line += 1
             if this[i] == '\t':
-                print("[line %d] found tab line" % line)
+                print("  [line %d] found tab line" % line)
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("usage : norme.py [C file]")
         sys.exit(1)
-
-    file_to_test = sys.argv[1]
-
-    if check_for_editor_header(file_to_test) != True:
-        print("[ERROR] ETNA header not found")
-    check_for_tabs(file_to_test)
-    check_for_spaces(file_to_test)
-    check_for_return(file_to_test)
+    for i in range(1, len(sys.argv)):
+        file_to_test = sys.argv[i]
+        print("file : %s" % sys.argv[i])
+        if check_for_editor_header(file_to_test) != True:
+            print("  [ERROR] ETNA header not found")
+        check_needed_spaces(file_to_test)
+        check_for_tabs(file_to_test)
+        check_for_spaces(file_to_test)
+        check_for_return(file_to_test)
