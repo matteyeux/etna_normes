dcd5a66ac314b39e57c9d81764279a499bc657ad
mathieu <mathieu.hautebas@gmail.com>
2019-01-18 15:06:55 +0100

    update script

--- normes.py
+++ normes.py
@@ -6,94 +6,91 @@
 ##     check for ETNA normes. Love u Samantha <3
 ##
 
-# TODO : open file once, not in every function
-
 import sys
 
 # In ETNA's pool you have to set a specific
 # header with your code
 
-def check_needed_spaces(c_file):
-    with open(c_file, "r") as f:
-        lines = f.readlines()
-        for i in range(0, len(lines)):
-            if "){" in lines[i]:
-                print("  [line %d] no space before curly bracket" % (i+1))
-            if "if(" in lines[i]:
-                print("  [line %d] no space between if and parenthese" % (i+1))
-            if "for(" in lines[i]:
-                print("  [line %d] no space between for and parenthese" % (i+1))
-            if "while(" in lines[i]:
-                print("  [line %d] no space between while and parenthese" % (i+1))
+def check_needed_spaces(line):
+	#for i in range(0, len(lines)):
+	if "){" in line:
+		print("  [line %d] no space before curly bracket" % (j+1))
+	elif "if(" in line:
+		print("  [line %d] no space between if and parenthese" % (j+1))
+	elif "for(" in line:
+		print("  [line %d] no space between for and parenthese" % (j+1))
+	elif "while(" in line:
+		print("  [line %d] no space between while and parenthese" % (j+1))
+	else :
+		pass
 
+def check_if_backline(line, line2):
 
-def check_for_editor_header(c_file):
-     with open(c_file, 'r') as f:
-        lines = f.readlines()
-        for i in range(0, len(lines)):
-            if "ETNA PROJECT," in lines[i]:
-                return True
-        return False
+	if "if" in line and ")\n" in line and "{" in line2 :
+		print("  [line %d] no curly bracket after parenthese" % (j+1))
+		print("  [line %d] curly bracket should be in the line above" % (j+2))
 
-def check_for_spaces(c_file):
-    with open(c_file, 'r') as f:
-        lines = f.readlines()
-        i = 0
-        for line in lines:
-            i += 1
-            if line.endswith(" \n"):
-                print("  [line %d] space at EoL" % i)
+types = ["int ", "float ", "char ", "void "]
 
-def check_for_return(c_file):
-    with open(c_file, "r") as f:
-         lines = f.readlines()
-         for i in range(0, len(lines)):
-            if "return" in lines[i]:
-                if "return;" in lines[i]:
-                    True
-                elif "return (" in lines[i]:
-                    True #print("good")
-                elif "return(" in lines[i]:
-                    print("  [line %d]  no space after return" % (i+1))
-                else:
-                    print("  [line %d]  no parentheses found for return" % (i+1))
+def check_curly_function(line):
+	found = 0
+	for type in types:
+		if type in line and "{" in line and found == 0:
+			found = 1
+			print("  [line %d] found curly bracket after function" % (j+1))
 
-def check_for_tabs(c_file):
-    with open(c_file, "r") as file:
-        this = file.read()
-        line = 0
-        for i in range(0, len(this)):
-            if this[i] == '\n':
-                line += 1
-            if this[i] == '\t':
-                print("  [line %d] found tab line" % line)
+def check_for_editor_header(line):
+	if "ETNA PROJECT," in line:
+		return True
+	return False
 
-def check_for_stdio(c_file):
-    with open(c_file, "r") as f:
-        lines = f.readlines()
-        for i in range(0, len(lines)):
-            if "stdio.h" in lines[i]:
-                print("  [line %d]  found stdio.h" % (i+1))
+def check_for_spaces(line):
+	if line.endswith(" \n"):
+		   print("  [line %d] space at EoL" % (j+1))
 
-def check_for_printf(c_file):
-    with open(c_file, "r") as f:
-        lines = f.readlines()
-        for i in range(0, len(lines)):
-            if "printf" in lines[i]:
-                 print("  [line %d]  found printf" % (i+1))
+def check_for_tabs(line):
+	if "\t" in line:
+		print("  [line %d] found tab" % (j+1))
+
+def check_for_stdio(line):
+	if "stdio.h" in line:
+		print("  [line %d] found stdio.h" % (j+1))
+
+def check_for_printf(line):
+	if "printf" in line:
+		print("  [line %d] found printf" % (j+1))
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("usage : normes.py [C file]")
-        sys.exit(1)
-    for i in range(1, len(sys.argv)):
-        file_to_test = sys.argv[i]
-        print("file : %s" % sys.argv[i])
-        if check_for_editor_header(file_to_test) != True:
-            print("  [ERROR] ETNA header not found")
-        check_needed_spaces(file_to_test)
-        if file_to_test != "Makefile":
-            check_for_tabs(file_to_test)
-        check_for_spaces(file_to_test)
-        check_for_stdio(file_to_test)
-        check_for_printf(file_to_test)
+	header = False
+	check_start = 1
+
+	if len(sys.argv) < 2:
+		print("usage : normes.py [C file]")
+		sys.exit(1)
+	if sys.argv[1] == "--hard" :
+		check_start = 2
+
+	for i in range(check_start, len(sys.argv)):
+		file_to_test = sys.argv[i]
+
+		with open(file_to_test, "r") as f:
+			print("file : %s" % sys.argv[i])
+			lines = f.readlines()
+
+			for j in range(0, len(lines)):
+				if check_for_editor_header(lines[j]) == True:
+					header = True
+
+				check_curly_function(lines[j])
+				check_needed_spaces(lines[j])
+				check_for_spaces(lines[j])
+				if check_start == 2:
+					check_for_stdio(lines[j])
+					check_for_printf(lines[j])
+					check_for_tabs(lines[j])
+
+				if j+1 < len(lines):
+					check_if_backline(lines[j], lines[j+1])
+
+			if header is False:
+				print("  [ERROR] ETNA header not found")
